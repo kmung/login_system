cebaea8a63ce8f63d74a6e2223b6bf113fb8c1c9
Khawm Mung <johnmungtawng5@gmail.com>
2022-07-19 18:33:45 -0500

    Create login.cpp

---
+++ login.cpp
@@ -0,0 +1,12 @@
+/*
+Author: Khawm Mung
+Description: Practicing C++ by creating a login app.
+/Date: 07/19/2022
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
