f9d57feb760652d43d6c8fb62e49e74d862ea053
Khawm Mung <johnmungtawng5@gmail.com>
2022-07-19 21:37:55 -0500

    path issue

--- login.cpp
+++ login.cpp
@@ -8,5 +8,15 @@ Description: Practicing C++ by creating a login app.
 #include <fstream>
 #include <string>
 #include <cstdlib>
+#include <cstdio>
 
 using namespace std;
+
+bool isLogin() {
+    // variables to store user info
+    string username;
+    string password;
+
+    //ask user to enter a username
+    cout << 'Enter username: ' << endl;
+}
